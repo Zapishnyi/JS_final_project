1ecb2c3f0ed90001fc518c4ed0d78e35afb48cc4
Zapishnyi <zapishny@ukr.net>
2024-03-30 23:25:36 +0200

    variable type correction

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 OKTEN school | JavaScript final project
-Originally I made transfer of search parameters using URL, but this will increase quantity of fetch request to server by 2 and potentially make UI little bit less pleasant. As long data already received I decided to passover it  through session storage. Alternative code is in comments.
+Originally I made transfer of search parameters using URL, but this will increase quantity of fetch 
+request to server by 2 and potentially make UI little bit less pleasant. 
+As long data already received I decided to passover it  through session storage.
+Alternative code is in comments.
--- render.js
+++ render.js
@@ -1,12 +1,12 @@
 // general
-let elm = (tegName) => document.createElement(tegName);
-let body = document.querySelector("body");
-let aTeg = elm("a");
+const elm = (tegName) => document.createElement(tegName);
+const body = document.querySelector("body");
+const aTeg = elm("a");
 let delay = 0;
 
 // Get data from database using provided URL and passover data to render function
 async function fetchRender(url, element, lookUp) {
-  let data = await fetch(url)
+  const data = await fetch(url)
     .then((response) => response.json())
     .then((data) => data)
     .catch((reject) => document.write(reject));
@@ -16,9 +16,9 @@ async function fetchRender(url, element, lookUp) {
 
 // function to render received data
 function render(data, element, lookUp) {
-  let dataWrap = elm("div");
+  const dataWrap = elm("div");
   dataWrap.classList.add(`level`);
-  // add idNo to class (to use it later for data manipulation)
+  // add idNo to class (to use it later for button event)
   if (data.id) {
     dataWrap.classList.add(`id${data.id}`, `id`);
   }
@@ -26,10 +26,11 @@ function render(data, element, lookUp) {
   element.appendChild(dataWrap);
   // crawling through received data
   for (const detail in data) {
+    // delay for fade effect
     delay = delay + 0.01;
     // check condition for detail to render or skip
-    let condition = lookUp.includes(detail) || lookUp.includes("all");
-    let dataString = elm("p");
+    const condition = lookUp.includes(detail) || lookUp.includes("all");
+    const dataString = elm("p");
     let value;
     // add step down sign in "value" if data contain object
     if (typeof data[detail] === "object") {
